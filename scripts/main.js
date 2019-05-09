af8b835fcbd30d480c31538e40db4debc45b75fd
Vineet Kumar <vineet.kumar@zensar.com>
2019-05-09 19:35:44 +0530

    code refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,30 @@
-
+// Tic Tac Toe v2
+var clickCount = 2;
+var winnerFound = false;
+var selectedTiles = [];
+var matrix = document.getElementById('matrix');
+var restart = document.getElementById('restart');
+var newGame = document.getElementById('new-game');
+var start = document.getElementById('start');
 var playerOneData = {
     name: null,
-    arr: []
+    checkList: []
 };
 var playerTwoData = {
     name: null,
-    arr: []
+    checkList: []
 };
-winnerFound = false;
-var clickCount = 2;
-
-var start = document.getElementById('start');
-var resultsList = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+var resultsList = [[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]];
+var setHTMLContent = function setHTMLContent(selector, value) {
+    document.querySelector(selector).innerHTML = value;
+}
 var hideShowElement = function (screenId, display) {
     //     hide user infor form
     document.getElementById(screenId).style.display = display;
@@ -35,69 +39,77 @@ var startGame = function () {
     //     show game board
     hideShowElement('game-board', 'block');
     //     set the player's name
-    document.querySelector('#p1-info span').innerHTML = playerOneData.name;
-    document.querySelector('#p2-info span').innerHTML = playerTwoData.name;
-}
+    setHTMLContent('#p1-info span', playerOneData.name);
+    setHTMLContent('#p2-info span', playerTwoData.name);
+};
 
 start.addEventListener('click', startGame);
 
-var ifClicked = function (index, tile) {
+var isTileChecked = function (index, tile) {
     var i = 0;
     while (i < tile.length) {
-        if (index == tile[i])
+        if (index == tile[i]) {
             return false;
+        }
         i++;
     }
-    return true
+    return true;
 };
-var selectedTiles = [];
+var showWinner = function showWinner(playersData) {
+    matrix.removeEventListener('click', startMatrix);
+    setHTMLContent('#result', "Winner is " + playersData.name);
+    winnerFound = true;
+    return true;
+}
 
-var winnerFinder = function (playersData, resultsList) {
-    if (playersData.arr.length < 2) return true;
-    var player = playersData.arr;
-    var i = 0;
-    resultsList.forEach(function (element) {
-        i = 0;
-        player.forEach(function (ele) {
-            if (element.indexOf(ele) >= 0) {
-                i++;
+var compareWithResultList = function compareWithResultList(playerCheckList, resultsList, playersData) {
+    resultsList.forEach(function (resultsListItem) {
+        var elementsMatchCount = 0;
+        // compare players array with ResultList sub array and check if any result matched
+        playerCheckList.filter(function (playerCheckListItem) {
+
+            if (resultsListItem.indexOf(playerCheckListItem) >= 0) {
+                elementsMatchCount++;
             }
         });
-        if (i == element.length) {
-            matrix.removeEventListener('click', startMatrix);
-            document.getElementById('result').innerHTML = "Winner is " + playersData.name;
-            winnerFound = true;
-            return false;
+        if (elementsMatchCount == resultsListItem.length) {
+            showWinner(playersData);
         }
     });
 };
-var matrix = document.getElementById('matrix');
+
+var winnerFinder = function (playersData) {
+    if (playersData.checkList.length < 2) return false;
+    var playerCheckList = playersData.checkList;
+    compareWithResultList(playerCheckList, resultsList, playersData);
+
+};
 var startMatrix = function ($event) {
     var target = $event.target;
     var tileIndex = +target.getAttribute('tile-index');
-    var alreadyClickedStatus = ifClicked(tileIndex, selectedTiles);
-    if (target.className === 'tile' && alreadyClickedStatus === true) {
+    var IsAlreadyChecked = isTileChecked(tileIndex, selectedTiles);
+    if (target.className === 'tile' && IsAlreadyChecked === true) {
         selectedTiles.push(tileIndex);
 
 
         if (clickCount % 2 === 0) {
-            playerOneData.arr.push(tileIndex);
+            playerOneData.checkList.push(tileIndex);
             target.className = 'tile red';
 
-            if (winnerFinder(playerOneData, resultsList) === false) {
+            if (winnerFinder(playerOneData, resultsList) === true) {
                 matrix.removeEventListener('click', startMatrix);
             }
         } else {
-            playerTwoData.arr.push(tileIndex);
+            playerTwoData.checkList.push(tileIndex);
             target.className = 'tile blue';
-            if (winnerFinder(playerTwoData, resultsList) === false) {
+            if (winnerFinder(playerTwoData, resultsList) === true) {
                 matrix.removeEventListener('click', startMatrix);
             }
         }
 
         clickCount++;
         if (clickCount === 11 && winnerFound === false) {
-            document.getElementById('result').innerHTML = 'Match drawn';
+            setHTMLContent('#result', 'Match drawn');
         }
     }
 }
@@ -106,21 +118,21 @@ var resetBoard = function () {
     winnerFound = false;
     clickCount = 2;
     matrix.addEventListener('click', startMatrix);
-    document.getElementById('result').innerHTML = '';
+    // remove match result and reset it to blank
+    setHTMLContent('#result', '');
     selectedTiles = [];
-    var tile = document.querySelectorAll('.tile')
+    var tile = document.querySelectorAll('.tile');
     tile.forEach(function (ele, index) {
         tile[index].className = 'tile';
     });
-    playerOneData.arr = [];
-    playerTwoData.arr = [];
+    playerOneData.checkList = [];
+    playerTwoData.checkList = [];
 };
-var restart = document.getElementById('restart');
+
 restart.addEventListener('click', function () {
 
     resetBoard();
 });
-var newGame = document.getElementById('new-game');
 newGame.addEventListener('click', function () {
     //     show user info form
     hideShowElement('user', 'block');
